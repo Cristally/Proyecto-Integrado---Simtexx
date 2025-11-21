016b8ffa0bc295a7f28f340f8d6fe573b46945b7
TrickyXIII <149970667+TrickyXIII@users.noreply.github.com>
2025-11-20 22:48:44 -0300

    db.js

--- Creacion OT/src/config/db.js
+++ Creacion OT/src/config/db.js
@@ -3,9 +3,10 @@ const { Pool } = require('pg');
 const pool = new Pool({
   user: 'postgres',       // Usuario por defecto de Postgres
   host: 'localhost',
-  database: 'simtexx_db', // La base que creamos en el Paso 1
-  password: 'Pi2025', // <--- ¡IMPORTANTE! Pon tu clave aquí
+  database: 'simtexx_db', // La base que creamos
+  password: '', // <--- ¡IMPORTANTE! Pon tu clave aquí
   port: 5432,
 });
+
 
 module.exports = pool;
